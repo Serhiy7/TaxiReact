54fca3cfeeea06c7c474fc12cf436f4ea035079f
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-05-30 00:28:42 +0200

    Miyagi

--- src/components/VideosOnYouTube/VideosOnYouTube.jsx
+++ src/components/VideosOnYouTube/VideosOnYouTube.jsx
@@ -1,38 +1,59 @@
 import styles from "./VideosOnYouTube.module.sass";
 
 /**
- * Top 5 Miyagi tracks in embedded YouTube iframes
+ * Компонент отображает ТОП-7 песен Miyagi в виде встроенных YouTube видео.
  * @component
  * @returns {JSX.Element}
  */
 const VideosOnYouTube = () => {
   const videos = [
     {
-      id: "WzQBAc8i73E", // I Got Love
-      title: "Miyagi & Эндшпиль feat. Рем Дигга – I Got Love",
+      id: "7pdN18RfGQw",
+      title: "Ollane feat. Miyagi - Touch The Sky (Official Audio)",
+    },
+
+    {
+      id: "4Ua1g8hU81A",
+      title: "Miyagi - Captain (2018)",
     },
     {
-      id: "WENR1e5yaXA", // Captain
-      title: "Miyagi – Captain (Official Video)",
+      id: "jJpKYZs6Av0",
+      title: "Miyagi - Самурай (Official Audio)",
     },
     {
-      id: "MNBcIBnU3a8", // Samurai
-      title: "Miyagi – Samurai (Official Video)",
+      id: "vN12KY7eR8U",
+      title: "Miyagi & Andy Panda - YAMAKASI (Official Video)",
     },
     {
-      id: "9Ri7TcukAJ8", // YAMAKASI
-      title: "Miyagi – YAMAKASI (Official Video)",
+      id: "xZEVGJszvo0",
+      title: "Miyagi & Andy Panda - Freeman (Official Video)",
     },
     {
-      id: "yeBvVvH4tfM", // Freeman
-      title: "Miyagi – Freeman (Official Video)",
+      id: "xZEVGJszvo0",
+      title: "Miyagi & Эндшпиль - Bounty (Official Audio)",
+    },
+    {
+      id: "DTz5k-8AzJo",
+      title: "Miyagi feat. Mav-d, Ollane - Music is Love (Official Audio)",
+    },
+    {
+      id: "iK7qs-1wTK8",
+      title: "Mr Lambo - Mango (Official Video)",
+    },
+    {
+      id: "af_Fnq39WQk",
+      title: "Miyagi & Эндшпиль - RudeBoys (Official Audio)",
+    },
+    {
+      id: "nidQCt_HEsY",
+      title: "Miyagi & Эндшпиль feat. Рем Дигга - I Got Love (Official Video)",
     },
   ];
 
   return (
     <section className={styles.videoSection} id="video">
-      <h2>Наша музыкальная подборка</h2>
-      <h3>ТОП-5 песен Miyagi:</h3>
+      <h2>Miyagi</h2>
+      <h3>ТОП песени :</h3>
 
       <div className={styles.videoWrapper}>
         <iframe

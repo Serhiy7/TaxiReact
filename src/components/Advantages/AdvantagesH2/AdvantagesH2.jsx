c09488b7c6a03b61ddb6cb983ba3aaa8411ad6a5
Serhiy7 <ssukalo79@gmail.com>
2025-05-29 21:30:51 +0200

    Advantages

---
+++ src/components/Advantages/AdvantagesH2/AdvantagesH2.jsx
@@ -0,0 +1,11 @@
+// import "./Advantages.css";
+
+const AdvaH2 = () => {
+  return (
+    <>
+      <h2>Наши преимущества</h2>
+    </>
+  );
+};
+
+export default AdvaH2;
---
+++ src/components/HomePage/Advantages/AdvantagesBox/AdvantagesBox.jsx
@@ -0,0 +1,34 @@
+// import "./Advantages.css";
+
+const AdvantagesBox = () => {
+  return (
+    <>
+      <div className="advantages-grid">
+        <div className="advantage-card">
+          <h3>Даем лучшие условия на рынке</h3>
+          <p>Нет залога и депозита на старте</p>
+          <p>Моментальный вывод, комиссия – 0,6%</p>
+        </div>
+        <div className="advantage-card">
+          <h3>С нами выгодно</h3>
+          <p>Нет штрафов за перепробег</p>
+          <p>На время сложного ремонта выдаем подменный авто</p>
+        </div>
+        <div className="advantage-card">
+          <h3>Работаем честно</h3>
+          <p>Простой и понятный договор</p>
+          <p>Нет скрытых платежей</p>
+        </div>
+        <div className="advantage-card">
+          <h3>С нами комфортно</h3>
+          <p>Поддержка 24/7 – отвечаем быстро и по делу</p>
+          <p>
+            Решаем споры с поддержкой Яндекса – мы отстаиваем позицию водителя
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdvantagesBox;

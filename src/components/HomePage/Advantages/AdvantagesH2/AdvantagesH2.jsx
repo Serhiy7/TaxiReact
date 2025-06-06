d5e75f68915664d7bdbfdde4dfd4c79707815f9e
Serhiy7 <ssukalo79@gmail.com>
2025-06-06 23:46:36 +0200

    2

--- src/components/HomePage/Advantages/AdvantagesH2/AdvantagesH2.jsx
+++ src/components/HomePage/Advantages/AdvantagesH2/AdvantagesH2.jsx
@@ -1,11 +1,7 @@
 // import "./Advantages.css";
 
 const AdvaH2 = () => {
-  return (
-    <>
-      <h2>Наши преимущества</h2>
-    </>
-  );
+  return <>{/* <h2>Наши преимущества</h2> */}</>;
 };
 
 export default AdvaH2;
